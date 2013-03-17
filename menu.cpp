4272295c8333c3636151afa131029aa85259c3f7
Max <silverhammermba@gmail.com>
2013-03-17 13:03:16 -0400

    Select menu entries using mouse or keyboard

--- menu.cpp
+++ menu.cpp
@@ -12,12 +12,21 @@ Menu::Menu(const std::string& ttl, const std::vector<std::string>& ents)
 	for (auto& entry : ents)
 	{
 		entries.push_back(sf::Text(entry, font, 30));
-		entries.back().setColor(sf::Color::White);
+		entries.back().setColor(sf::Color(150, 150, 150));
 		bounds = entries.back().getGlobalBounds();
 		pos.x = -bounds.width / 2;
 		pos.y += bounds.height * 1.5;
 		entries.back().setPosition(pos);
 	}
+
+	selected = 0;
+	select(0);
+}
+
+void Menu::select(unsigned int i)
+{
+	entries[selected].setColor(sf::Color(150, 150, 150));
+	entries[selected = i].setColor(sf::Color::White);
 }
 
 void Menu::draw_on(sf::RenderWindow& window) const
@@ -34,7 +43,22 @@ bool Menu::process_event(sf::Event& event)
 		case sf::Event::KeyPressed:
 			if (event.key.code == sf::Keyboard::Escape)
 				finished = true;
+			else if (selected > 0 && event.key.code == sf::Keyboard::Up)
+				select(selected - 1);
+			else if (selected < entries.size() - 1 && event.key.code == sf::Keyboard::Down)
+				select(selected + 1);
 			break;
+		case sf::Event::MouseMoved:
+		{
+			sf::Vector2f mouse {(float)event.mouseMove.x, (float)event.mouseMove.y};
+			for (unsigned int i = 0; i < entries.size(); i++)
+				if (entries[i].getGlobalBounds().contains(mouse))
+				{
+					select(i);
+					break;
+				}
+			break;
+		}
 		default:
 			break;
 	}
--- menu.hpp
+++ menu.hpp
@@ -2,8 +2,11 @@ class Menu : public InputReader
 {
 	sf::Text title;
 	std::vector<sf::Text> entries;
+	unsigned int selected;
+	void select(unsigned int i);
 public:
 	Menu(const std::string& ttl, const std::vector<std::string>& ents);
+
 
 	void draw_on(sf::RenderWindow& window) const;
 
