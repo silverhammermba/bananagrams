f032b3da8b90e9bd192fb78a5ee1dc18f3f7c573
Max <silverhammermba@gmail.com>
2013-04-01 17:30:07 -0400

    Reformat letter counts

--- bananagrams.hpp
+++ bananagrams.hpp
@@ -25,32 +25,8 @@ extern sf::View gui_view;
 
 static const unsigned int letter_count[26] =
 {
-	13,
-	3,
-	3,
-	6,
-	18,
-	3,
-	4,
-	3,
-	12,
-	2,
-	2,
-	5,
-	3,
-	8,
-	11,
-	3,
-	2,
-	9,
-	6,
-	9,
-	6,
-	3,
-	3,
-	2,
-	3,
-	2
+	13, 3, 3, 6, 18, 3, 4, 3, 12, 2, 2, 5, 3, 8, 11, 3, 2, 9, 6, 9, 6, 3, 3, 2, 3, 2
+//  A   B  C  D  E   F  G  H   I  J  K  L  M  N   O  P  Q  R  S  T  U  V  W  X  Y  Z
 };
 
 // insert x into list l at a random position
