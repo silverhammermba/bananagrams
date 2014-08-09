f2ddb51287f53bd3400a88f5e73a2fb51fd47b0e
Max <silverhammermba@gmail.com>
2014-08-09 10:25:23 -0400

    add script for getting DLLs for windows

---
+++ get_dlls_.rb
@@ -0,0 +1,32 @@
+$dlls = {}
+$missing = {}
+
+def get_dlls file
+  `x86_64-w64-mingw32-objdump -x #{file}`.each_line do |line|
+    if line =~ /DLL Name: (.*)/
+      dll = $1
+      # find actual location of DLL
+      unless File.exists? dll
+        lib = File.join('/usr/x86_64-w64-mingw32/bin', dll)
+        if File.exists? lib
+          dll = lib
+        else
+          $missing[dll] = true
+          next
+        end
+      end
+
+      unless $dlls[dll]
+        $dlls[dll] = true
+        get_dlls dll
+      end
+    end
+  end
+end
+
+ARGV.each do |arg|
+  get_dlls arg
+end
+
+puts $dlls.keys.sort
+STDERR.puts "Missing", $missing.keys.sort
